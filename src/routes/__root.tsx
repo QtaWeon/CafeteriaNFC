cee33e8ebc949210bf3ee634c481e22dd39d8462
Giovanni Portillo <giovanniportillo2019@gmail.com>
2026-09-23 19:26:08 -0300

    fix: load CSS correctly in SPA mode, add Tailwind v4 Vite plugin

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet, Link, createRootRouteWithContext, useRouter } from "@tanstack/react-router";
 import { useEffect } from "react";
-
-import appCss from "../styles.css?url";
 import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
